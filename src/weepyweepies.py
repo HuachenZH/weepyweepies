18be201a1fb16c2f11db47c2056a10b50f4409a3
HuachenZH <huachen.zhang.23@eigsi.fr>
2024-03-07 22:24:56 +0100

    add docstring

--- src/weepyweepies.py
+++ src/weepyweepies.py
@@ -34,11 +34,39 @@ def shitty_edge_detection(path_img:str) -> np.ndarray:
 
 
 def array2scatter(edges:np.ndarray) -> tuple[np.ndarray, np.ndarray]:
+    """Extract coordinate of scatter points from an image's array.
+ 
+            Parameters:
+                    edges (np.ndarray)): array of the image.
+                    The image is in greyscale in black background, all pixels in white,
+                    their row and col index will be extracted. 
+ 
+            Returns:
+                    (tuple[np.ndarray, np.ndarray]): a tuple of two numpy arrays.
+                    The first array is the index of row of white pixels.
+                    The second array is the index of column of white pixels.
+    """
     return np.where(edges > 250)
 
 
 
-def smash_on_plane_xz(edges):
+def smash_on_plane_xz(edges:np.ndarray) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
+    """Project an image onto the xz plane.
+ 
+            Parameters:
+                    edges (np.ndarray)): array of the image.
+ 
+            Returns:
+                    xs, ys, zs (tuple[np.ndarray, np.ndarray, np.ndarray]): a tuple of three numpy arrays.
+                    xs: array of scatter points' x axis coordinate.
+                    ys: array of scatter points' y axis coordinate.
+                    zs: array of scatter points' z axis coordinate.
+                    ----------------
+                    xs: (1, 7, 6, 7, 9)
+                    ys: (3, 4, 9, 7, 0)
+                    zs: (0, 1, 6, 5, 1)
+                         ^  Coordinate of the point: (1, 3, 0)
+    """
     zs, xs = array2scatter(edges)
     ys = np.zeros(len(zs), dtype=int)
     return xs, ys, zs
@@ -46,7 +74,7 @@ def smash_on_plane_xz(edges):
 
 
 def main():
-    path_img = "../data/txt_quentin.jpg"
+    path_img = "../data/txt_anae.jpg"
     edges = shitty_edge_detection(path_img)
     xs, ys, zs = smash_on_plane_xz(edges)
     # Prepare for plot
