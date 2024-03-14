c31c509a2554b1aeb3f0de444c8c5bb3ce01cad6
HuachenZH <huachen.zhang.23@eigsi.fr>
2024-03-14 23:52:08 +0100

    building doppel

--- src/tamaki_iroha.py
+++ src/tamaki_iroha.py
@@ -22,10 +22,24 @@ def scale_down(arr_img:np.ndarray) -> np.ndarray:
 
 
 
+def doppel(arr_img:np.ndarray) -> np.ndarray:
+    """沈黙のドッペル !! Transform magical girl into doppel"""
+    dict_doppel = {}
+    dict_doppel[0] = np.ones((3,3))
+    dict_doppel[1] = np.array(([1,0,1], [0,1,0], [1,0,1]))
+    dict_doppel[2] = np.array(([0,1,0], [1,0,1], [0,1,0]))
+    dict_doppel[3] = np.array(([0,0,0], [1,1,1], [0,0,0]))
+    dict_doppel[4] = np.array(([0,0,0], [0,1,0], [0,0,0]))
+    dict_doppel[5] = np.zeros((3,3))
+    breakpoint()
+
+
+
 def main():
     path_input_img = "../data/anae_small.jfif"
     arr_img = cv2.imread(path_input_img, flags=0)  
     arr_img = scale_down(arr_img)
+    doppel(arr_img)
 
 
 
