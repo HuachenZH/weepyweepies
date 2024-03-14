191c0b162030026913d7aaba7b0a14bd722d9c09
HuachenZH <huachen.zhang.23@eigsi.fr>
2024-03-14 23:21:57 +0100

    build fct scale_down

--- src/tamaki_iroha.py
+++ src/tamaki_iroha.py
@@ -1,8 +1,34 @@
 """Transform image to matrice. Each pixel becomes a matrix of 3*3"""
 import numpy as np
 import cv2
+import pdb
 
 
 
 
+def scale_down(arr_img:np.ndarray) -> np.ndarray:
+    """Calculate the level of each pixel.
+ 
+            Parameters:
+                    arr_img (np.ndarray): array of image in greyscale.
+ 
+            Returns:
+                    arr_img (np.ndarray): array scaled down.
+                    By default there are 6 levels in total, 
+                    so the output array is composed by int between 0 and 5 (included).
+    """
+    notch = 255 / 6
+    return arr_img // notch
+
+
+
+def main():
+    path_input_img = "../data/anae_small.jfif"
+    arr_img = cv2.imread(path_input_img, flags=0)  
+    arr_img = scale_down(arr_img)
+
+
+
+if __name__ == "__main__":
+    main()
 
